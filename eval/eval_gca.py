2fdba704ef414a9057f14fc7bc70d765c508d0a5
stefano-marchesin <34963894+stefano-marchesin@users.noreply.github.com>
2023-02-02 17:43:59 +0100

    update eval_gca.py

--- eval/eval_gca.py
+++ eval/eval_gca.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import time
 import math
 import torch
@@ -11,6 +12,7 @@ from sklearn.model_selection import StratifiedKFold, train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report
 from transformers import BertTokenizer, BertForSequenceClassification, BertConfig, AdamW, get_linear_schedule_with_warmup
 
+sys.path.append('.')
 from src.models import utils
 
 
